436dee30c7ab82a9af135383c2be2148288d9fb9
Ibrahimsuf <110345830+Ibrahimsuf@users.noreply.github.com>
2023-10-20 14:35:17 -0500

    Allow user to enter pdb id from command line

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from driver import Driver
 from  annotations_parser import Annotations_parser
 import pandas as pd
+import sys
+
+
 def main():
-    driver = Driver("5fj8")
+    driver = Driver(sys.argv[1])
 
     annotations_html = driver.get_all_annotations()
 
     parser = Annotations_parser(annotations_html)
 
     parser.get_annotations()
-    parser.write_annotations_to_csv("test.csv") 
-main()
+    parser.write_annotations_to_csv(sys.argv[2]) 
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        main()
+    else:
+        print("python main.py <pdb id> <output_file.csv>")
